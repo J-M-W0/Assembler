d4771682c0bf4b5709deda861f59fcb989994c3f
JMWang <junzhe2002@yahoo.com>
2024-06-22 23:00:05 +0200

    neat code

--- assembler.c
+++ assembler.c
@@ -3,13 +3,6 @@
 #include "str.h"
 #include "table.h"
 #include "instru.h"
-#include <stdio.h>
-
-extern char * in;
-extern char * out;
-extern TTable * LabelTable;
-extern FILE * In;
-extern FILE * Out;
 
 int main(int argc, const char * const argv[]) {
     init(argc, argv);
